4c9a92862c762d47b2849e0abd4e71596dd23327
shadowbeat274 <olivertate274@gmail.com>
2021-09-15 20:50:37 -0500

    practice webpack

--- webpacksampleTS/webpack.config.js
+++ webpacksampleTS/webpack.config.js
@@ -1,28 +1,52 @@
-const { resolve } = require( 'path' )
+// const { resolve } = require( 'path' )
+
+// module.exports = {
+//   entry: './src/index.ts',
+//   output: {
+//     filename: 'index_typescript_export.js',
+//     path: resolve( __dirname, 'dist' )
+//   },
+//   module: {
+//     rules: [
+//     {
+//       test: /\.ts$/,
+//       exclude: /node_modules/,
+//       use: 'ts-loader'
+//     }
+//   ]
+//   },
+//   resolve: {
+//     extensions:[ '.ts' , '.js' ]
+//   },
+//   target: 'node',
+//   mode: 'development',
+//   devServer: {
+//     static: {
+//       directory: resolve( __dirname, '' )
+//     }
+//   }
+// }
+
+const { resolve } =  require( 'path' )
 
 module.exports = {
   entry: './src/index.ts',
   output: {
-    filename: 'index_typescript_export.js',
-    path: resolve( __dirname, 'dist' )
+    filename: 'index_compiled.js',
+    path: resolve( __dirname, 'dist')
   },
   module: {
     rules: [
-    {
-      test: /\.ts$/,
-      exclude: /node_modules/,
-      use: 'ts-loader'
-    }
-  ]
+      {
+       test: /\.ts/, 
+       exclude: /node_modules/,
+       use: 'ts-loader'
+      }
+    ]
   },
   resolve: {
-    extensions:[ '.ts' , '.js' ]
+    extensions: [ '.ts', '.js' ]
   },
   target: 'node',
-  mode: 'development',
-  devServer: {
-    static: {
-      directory: resolve( __dirname, '' )
-    }
-  }
+  mode: 'production'
 }
