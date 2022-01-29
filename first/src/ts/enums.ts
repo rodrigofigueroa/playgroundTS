0837f29cf03f4112c140989bc44720021bf77534
shadowbeat274 <olivertate274@gmail.com>
2022-01-29 09:35:56 -0600

    enums

---
+++ first/src/ts/enums.ts
@@ -0,0 +1,11 @@
+enum WeekDays {
+  Monday = 1, // could be the next number
+  Tuesday , // could be the next number
+  Wensday , // could be the next number
+  Thursday , // could be the next number
+  Friday , // could be the next number
+  Saturday , // could be the next number
+  Sunday, // could be the next number
+}
+
+console.log( WeekDays.Saturday )
